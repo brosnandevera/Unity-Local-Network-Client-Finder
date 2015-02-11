afd6302eb7588a0c00bd14aebfda6e5ee2924451
brosnan de vera <axsyon@gmail.com>
2015-02-11 22:38:28 +0800

    added cube move code

---
+++ Assets/CubeMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Move Left");
+            transform.position = new Vector3(transform.position.x - 0.05f, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Move Left");
+            transform.position = new Vector3(transform.position.x + 0.05f, transform.position.y, transform.position.z);
+        }
+       
+	}
+}
