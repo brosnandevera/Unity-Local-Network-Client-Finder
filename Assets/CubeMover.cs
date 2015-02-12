b584769fff83ca37fb9292cbfff0097b258f473e
brosnan de vera <axsyon@gmail.com>
2015-02-12 16:31:21 +0800

    test SerializeNetworkView

--- Assets/CubeMover.cs
+++ Assets/CubeMover.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CubeMover : MonoBehaviour {
 
 	// Use this for initialization
+    public int testVal = 0;
 	void Start () {
 	
 	}
@@ -17,9 +18,28 @@ public class CubeMover : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Move Left");
+            Debug.Log("Move Right");
             transform.position = new Vector3(transform.position.x + 0.05f, transform.position.y, transform.position.z);
         }
        
 	}
+
+    public void OnSerializeNetworkView(NetworkMessageInfo info, BitStream stream)
+    {
+        if (stream.isWriting)
+        {
+           float val =  testVal;
+           stream.Serialize(ref val);
+           
+        }
+        else
+        {
+            //reading
+            float val2 = 0;
+            stream.Serialize(ref val2);
+            Debug.Log("Lol "+ testVal);
+        }
+    }
+
+
 }
